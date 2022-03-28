c177fb66e7bc3d92f2e7a6224e98be8d247edc5d
ianashby <ianashbyphotography@gmail.com>
2022-03-28 17:18:53 -0600

    constants update

--- saltdew_valley/constants.py
+++ saltdew_valley/constants.py
@@ -1,9 +1,23 @@
+
+from game.shared.color import Color
+
+# -------------------------------------------------------------------------------------------------- 
+# GENERAL GAME CONSTANTS
+# -------------------------------------------------------------------------------------------------- 
+
+# Game
 CAPTION = "Saltdew Valley"
+FRAME_RATE = 15
+
+# Screen
 MAX_X = 1185
 MAX_Y = 455
+
+
 CELL_SIZE = 16
 FONT_SIZE = 15
 COLUMNS = 40
 ROWS = 20
-FRAME_RATE = 15
+
+# Sprites
 FARMER = "assets/Farmer_001.png"
