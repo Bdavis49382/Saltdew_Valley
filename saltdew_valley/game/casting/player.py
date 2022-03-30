48108c6d510d0155c495092a45245cff21bb2ae2
Bdavis49382 <bendavis493@gmail.com>
2022-03-29 20:28:16 -0600

    Added displays,aging and gameover

--- saltdew_valley/game/casting/player.py
+++ saltdew_valley/game/casting/player.py
@@ -19,3 +19,33 @@ class Player(Actor):
         self._texture = FARMER
         self._position = Tile(FARMER_STARTING_X,FARMER_STARTING_Y)
         self._velocity = Tile(0,0)
+        self._age = 20
+        self._clock = 0
+        self._salt = 200
+    
+    def move_next(self):
+        super().move_next()
+        self._clock+=1
+        if(self._clock%FAST_AGING==0):
+            self._age+=1
+        
+    def calculate_max_speed(self):
+        if self._age<40:
+            return .5
+        elif self._age<50:
+            return .4
+        elif self._age<60:
+            return .3
+        return .25/(.2*(self._age-55))
+
+    def get_age(self):
+        return self._age
+    
+    def set_age(self,age):
+        self._age = age
+    
+    def get_salt(self):
+        return self._salt
+    
+    def set_salt(self,salt):
+        self._salt = salt
--- saltdew_valley/game/scripting/handle_collisions_action.py
+++ saltdew_valley/game/scripting/handle_collisions_action.py
@@ -1,4 +1,21 @@
 from game.scripting.action import Action
-
+from game.casting.text_bar import Text_bar
+from game.shared.tile import Tile
 class Handle_collisions_action(Action):
-    pass
+
+    def __init__(self) -> None:
+        super().__init__()
+    
+
+
+    def execute(self,cast,script):
+        self.check_for_game_over(cast)
+        
+
+    def check_for_game_over(self,cast):
+        player = cast.get_first_actor("players")
+        snail = cast.get_first_actor("snails")
+
+        if player.get_position().equals(snail.get_position()):
+
+            cast.add_actor("text_bars",Text_bar("Game Over ",Tile(1,4)))
